e0ca12f75e29ea467ef12ac72986715f52ddb802
kai.chen <kai.chen@relaistech.net>
2023-01-27 16:40:00 +0800

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				blob: "blob 7s infinite"
+			},
+			keyframes: {
+				blob: {
+					"0%": {
+						transform: "translate(0px, 0px) scale(1)"
+					},
+					"33%": {
+						transform: "translate(30px, -50px) scale(1.1)"
+					},
+					"66%": {
+						transform: "translate(-20px, 20px) scale(0.9)"
+					},
+					"100%": {
+						transform: "translate(0px, 0px) scale(1)"
+					},
+				}
+			}
+		},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
